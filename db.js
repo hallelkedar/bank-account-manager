8b9bce7465e8463d16d474f690c899ebd005ab56
Hallel <hallelkedar000@gmail.com>
2026-06-25 16:06:43 +0300

    Fix db function

--- db.js
+++ db.js
@@ -1,39 +1,45 @@
-const BankAccounts = []
+import { createCustomer } from "./customersManager.js";
+const BankAccounts = [];
 
 export function addAccount(account) {
-    BankAccounts.push(account)
+  BankAccounts.push(account);
 }
 
 export function getAllAccounts() {
-    return Object.entries(BankAccounts)
+  if (!BankAccounts) console.log("WARNING | there is no customers in the bank");
+  return BankAccounts.map((account) => {
+    (account.id, account.accountType, account.balance, account.isActive);
+  });
 }
 
 export function getAccount(id) {
-    return BankAccounts.find(acc => acc.id === id)
+  return BankAccounts.find((acc) => acc.id === id);
 }
 
 export function getAccountByName(name) {
-    const accName = name.trim().toLowerCase()
-    return BankAccounts.filter(
-        acc => acc.fullName.trim().toLowerCase() === accName
-    )
+  const accName = name.trim().toLowerCase();
+  return BankAccounts.filter(
+    (acc) => acc.fullName.trim().toLowerCase() === accName,
+  );
 }
 
 export function getStatistics() {
-    return {
-        totalCustomers: BankAccounts.length,
-        activeAccounts: BankAccounts.reduce(
-            (total, account) => account.isActive ? total+1 : total, 0
-        ),
-        totalMoney: BankAccounts.reduce(
-            (total, account) => total + account.balance, 0
-        ),
-        getAvargeBalance() {
-            return this.totalMoney / this.activeAccounts
-        },
-        highestBalance: BankAccounts.reduce(
-            (max, account) => account.balance > max ? max = account.balance : max, 0
-        )
-
-        }
-    }
+  return {
+    totalCustomers: BankAccounts.length,
+    activeAccounts: BankAccounts.reduce(
+      (total, account) => (account.isActive ? total + 1 : total),
+      0,
+    ),
+    totalMoney: BankAccounts.reduce(
+      (total, account) => total + account.balance,
+      0,
+    ),
+    get averageBalance() {
+      return (this.totalMoney / this.activeAccounts) || 0;
+    },
+    highestBalance: BankAccounts.reduce(
+      (max, account) => (account.balance > max ? (max = account.balance) : max),
+      0,
+    ),
+  };
+}
