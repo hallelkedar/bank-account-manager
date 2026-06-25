7188f2d875327a81dba4437de8f907bda7c1c829
Hallel <hallelkedar000@gmail.com>
2026-06-25 16:08:18 +0300

    Added manager, create new customer and get customer details clean

---
+++ customersManager.js
@@ -0,0 +1,49 @@
+import {
+  IdGenerator,
+  createAccountIsValid,
+  atmValidation,
+} from "./utils/utils.js";
+import { getAllAccounts, getAccount, getAccountByName } from "./db.js";
+
+export function createCustomer(fullName, accountType, initialBalance) {
+  if (createAccountIsValid(fullName, accountType, initialBalance)) {
+    return {
+      id: IdGenerator(),
+      fullName,
+      accountType,
+      balance: initialBalance,
+      isActive: true,
+      deposit: function (amount) {
+        if (!atmValidation(amount, "deposit")) {
+          return false;
+        }
+        this.balance += amount;
+        return true;
+      },
+      withdraw: function (amount) {
+        if (!atmValidation(amount, "withraw")) {
+          return false;
+        }
+        this.balance -= amount;
+        return true;
+      },
+      closeAccount() {
+        if (!this.isActive) {
+          return "Account is already close.";
+        }
+        this.isActive = false;
+        return "Account closed successfully";
+      },
+    };
+  }
+  return null
+}
+
+export function getCustomerArray(customer) {
+    return Object.fromEntries(
+    Object.entries(customer).filter(
+      ([key, value]) => typeof value !== "function",
+    ),
+  );
+}
+
